0d9a73003b370cfc101eaac5e416b6e9af0c5408
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-18 00:16:14 +0400

    add basic app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import { Input } from 'antd';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+import Typograf from 'typograf';
+import { Typography } from 'antd';
+import Header from './Header';
+import {CopyOutlined} from '@ant-design/icons'
+
 
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
+  const [text, setText] = useState('');
+
+  const { TextArea } = Input;
+  const { Text } = Typography;
+
+  const onChange = (e) => {
+    setText(e.target.value);
+    setIsVisible(false);
+  };
+
+  const handleClick = () => {
+    setIsVisible(true);
+    var tp = new Typograf({ locale: ['ru', 'en-US'] });
+    setText(tp.execute(text));
+    if (text === '') {
+      alert('Введите, пожалуйста, текст! Поле не может быть пустым.');
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {}, [text]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <TextArea
+        autoFocus={true}
+        showCount
+        allowClear={true}
+        bordered={true}
+        className='input1'
+        onChange={onChange}
+        placeholder='Скопируйте сюда ваш шедевр мировой литературы'
+      />
+
+      <Button onClick={handleClick} type='primary' className='button1'>
+        Подготовить текст к публикации
+      </Button>
+
+      {isVisible && (
+        <div className='container2'>
+        <TextArea
+          readOnly
+          autoFocus={true}
+          showCount
+          allowClear={true}
+          bordered={true}
+          className='input2'
+          placeholder='Скопируйте сюда ваш шедевр мировой литературы'
+          value={text}
+        />
+        <Button type='primary' icon={<CopyOutlined/>} className='button2'/>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <h1 className='header__title'>
+        <span className='text2'>Электронный помощник писателя</span>
+     
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
