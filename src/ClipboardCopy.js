95c24b86b3da7eb4c40ef6c1ca3c932d6986c3df
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-18 11:47:57 +0400

    new commit

--- src/ClipboardCopy.js
+++ src/ClipboardCopy.js
@@ -15,3 +15,18 @@ export default function ClipboardCopy({ copyText }) {
     }
   }
 
+  // onClick handler function for the copy button
+  const handleCopyClick = () => {
+    // Asynchronously call copyTextToClipboard
+    copyTextToClipboard(copyText)
+      .then(() => {
+        // If successful, update the isCopied state value
+        setIsCopied(true);
+        setTimeout(() => {
+          setIsCopied(false);
+        }, 1500);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
