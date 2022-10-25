00b8ae7ba7f07ef551dd8e21c516a3211b55f4ed
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-25 22:48:23 +0400

    new commit

---
+++ src/modules/RuleEmDash.ts
@@ -0,0 +1,16 @@
+// Подставляет тире, не внося ошибок в Markdown
+
+import Typograf from 'typograf';
+
+const Rule1 = () => {
+    Typograf.addRule({
+      name: 'common/other/typographicSmiley',
+      handler: function (text) {
+        return text.replace(/:-\)/g, ':-D');
+      },
+    });
+    return null;
+  };
+  
+  export default Rule1;
+  
